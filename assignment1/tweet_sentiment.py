b3960465d1436afc5b36062aa624da94f3499e33
Huong Nguyen <huong.nguyen@ubitec.ch>
2018-08-18 14:22:31 +0700

    problem 2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,43 @@
 import sys
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def load_sent(filename):
+    sent_file = open(filename)
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    return scores.items()
+
+
+def load_tweet(filename):
+    import json
+
+    with open(filename) as f:
+        tweets = f.readlines()
+
+    return [json.loads(tweet) for tweet in tweets]
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sentiment_file = sys.argv[1]
+    tweet_file = sys.argv[2]
+
+    sentiment_dict = load_sent(sentiment_file)
+    tweet_list = load_tweet(tweet_file)
+    terms = [x[0] for x in sentiment_dict]
+
+    for tweet in tweet_list:
+        encoded_tweet = tweet["text"]
+        words = encoded_tweet.split()
+        scores = set(words).intersection(terms)
+        if len(scores) == 0:
+            print 0
+        else:
+            total = [x[1] for x in sentiment_dict if x[0] in scores]
+            print sum(total)
+
 
 if __name__ == '__main__':
     main()
